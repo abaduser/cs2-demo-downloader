c846a022f995f6df88d4a66ad082c4d22e1d9dbf
the jukebox <5196313+TheJukebox@users.noreply.github.com>
2024-05-26 09:01:05 +1000

    Code quality with black

--- client.py
+++ client.py
@@ -5,7 +5,13 @@ import re
 
 SETTINGS_FILE = "settings.toml"
 SETTINGS_TEMPLATE = {
-    "Settings": {"community_id": "", "match_types_to_download": ["premier"], "download_behavior": "periodic", "download_interval": "daily", "download_location": "", "download_"},
+    "Settings": {
+        "community_id": "",
+        "match_types_to_download": ["premier"],
+        "download_behavior": "periodic",
+        "download_interval": "daily",
+        "download_location": "",
+    },
 }
 
 
@@ -23,7 +29,7 @@ def generate_new_settings():
     setting_comments = [
         "# Make sure to set this is set to the type of matches you want to track (premier, competitivepermap, scrimmage, wingman)",
         "# Download behavior dictates when to download your demos 'periodic', 'startup' and 'manual'",
-        "# Intervals available are daily / hourly"
+        "# Intervals available are daily / hourly",
     ]
     new_settings = SETTINGS_TEMPLATE
     new_settings["Settings"]["community_id"] = prompt_empty_community_id()
